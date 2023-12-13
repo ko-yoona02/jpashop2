56c46e2b36d3ea21d4c7234df113d0746cbbdc19
ko-yoona02 <gmlhee99@gmail.com>
2023-12-13 15:43:42 +0900

    jpashop22

---
+++ jpashop22/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
+
+//    @Bean
+//    Hibernate5Module hibernate5Module() {
+//        return new Hibernate5Module();
+//    }
+
+}
